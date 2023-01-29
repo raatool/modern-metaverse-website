77771af2738f7a9ecc8286c22b6260d4a54b4695
raatool <46068313+raatool@users.noreply.github.com>
2023-01-29 07:54:37 +0200

    No ESLint - update

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,9 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "next/core-web-vitals"
   ],
   parserOptions: {
     ecmaFeatures: {
